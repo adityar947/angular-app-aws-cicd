26d4816df0d340fe5154c9ded6a221e41720093b
“adityar947” <adityar947@gmail.com>
2023-10-06 11:16:07 +0530

    +Added buildspec.yml appspec.yml and scripts

---
+++ scripts/after_install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# navigate to app folder
+cd /app
+
+# Copy artifact to destination
+cp -r build/* /var/www/html
---
+++ scripts/before_install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# navigate to app folder
+cd /app
+
+# install node, npm and Angular@CLI
+apt install curl
+curl -fsSL https://deb.nodesource.com/setup_18.x | sudo -E bash -
+apt install nodejs -y
+apt install npm -y
+npm install -g @angular/cli -y
+sudo apt install nginx -y
+ufw allow 'Nginx HTTP'
+sudo systemctl start nginx
