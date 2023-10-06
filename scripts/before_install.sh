7373f552f74b7feb1d3bf2e660b808783cd65450
Aditya Ranjan <93814011+adityar947@users.noreply.github.com>
2023-10-06 15:20:20 +0530

    Update before_install.sh

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -2,13 +2,13 @@
 
 # navigate to app folder
 cd /app
-
+pwd
 # install node, npm and Angular@CLI
-apt install curl
-curl -fsSL https://deb.nodesource.com/setup_18.x | sudo -E bash -
-apt install nodejs -y
-apt install npm -y
-npm install -g @angular/cli -y
-sudo apt install nginx -y
-ufw allow 'Nginx HTTP'
-sudo systemctl start nginx
+# apt install curl
+# curl -fsSL https://deb.nodesource.com/setup_18.x | sudo -E bash -
+# apt install nodejs -y
+# apt install npm -y
+# npm install -g @angular/cli -y
+# sudo apt install nginx -y
+# ufw allow 'Nginx HTTP'
+# sudo systemctl start nginx
